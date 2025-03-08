667e71c478fc8577f550359a6382ee2278ee6aea
Madhu Sudan <madhus1297@gmail.com>
2025-03-08 11:40:26 +0530

    This changes

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def start_timer():
     long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
-        timer_label.config(text="Break1", font=("Courier", 50, "bold"), fg=RED, bg=YELLOW, highlightthickness=0)
+        timer_label.config(text="Break", font=("Courier", 50, "bold"), fg=RED, bg=YELLOW, highlightthickness=0)
         count_down(long_break_sec)
     elif reps % 2 == 0:
-        timer_label.config(text="Break", font=("Courier", 50, "bold"), fg=PINK, bg=YELLOW, highlightthickness=0)
+        timer_label.config(text="Breakeven", font=("Courier", 50, "bold"), fg=PINK, bg=YELLOW, highlightthickness=0)
         count_down(short_break_sec)
     else:
         timer_label.config(text="Work", font=("Courier", 50, "bold"), fg=GREEN, bg=YELLOW, highlightthickness=0)
